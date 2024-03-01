f83a218c46d7ee17587da8f91c99e5963e1a8b97
Dathon Weber <dathonweber82@gmail.com>
2024-02-29 20:51:41 -0600

    removed gsap trial from app and added image to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react'
-import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters'
-//import Logo from './Logo'
+import Logo from './Logo'
 import './index.scss'
 import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const nameArray = [' ','D','a', 't', 'h', 'o', 'n']
   const jobArray = [
     'w',
     'e',
@@ -41,9 +40,9 @@ const Home = () => {
             <span className={letterClass}>H</span>
             <span className={`${letterClass} _12`}>i,</span>
             <br />
-            <span className={`${letterClass} _13`}>I,</span>
+            <span className={`${letterClass} _13`}>I</span>
             <span className={`${letterClass} _14`}>'m</span>
-            <img src={LogoTitle} alt="developer" />
+            
             <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
@@ -56,12 +55,12 @@ const Home = () => {
               idx={22}
             />
           </h1>
-          <h2> Frontend Developer / JavaScript Expert / YouTuber</h2>
+          <h2> Full Stack Developer / JavaScript Expert / Salesforce Commerce Cloud Expert</h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME{' '}
           </Link>
         </div>
-        {/* <Logo /> */}
+        <Logo />
       </div>
       <Loader type='ball-pulse-sync' />
     </>
