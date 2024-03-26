69c4a422badeb852bc72f6239aa2b82dfd08b1b5
Dathon Weber <dathonweber82@gmail.com>
2024-03-25 19:19:21 -0500

    Changed my mywork tab to a bulleted list and added that list back to the mobile display

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -10,7 +10,12 @@ class Project extends React.Component {
             <div className='project'>
                 <div className='project-left'>
                     <a href={projectURL} className='project-url' target='_blank' rel='noreferrer'><h1 className='project-name'>{projectName}</h1></a>
-                    <p className='project-description'>{description}</p>
+                    <p className='project-contribution'>What I contributed to the project:</p>
+                    <ul className='project-description'>
+                        {description.map((line, index) => (
+                            <li key={index}>{line}</li>
+                        ))}
+                    </ul>
                 </div>
                 <div className='project-right'>
                     <ImageCarousel imageUrls={projectImages} />
--- src/projectData.js
+++ src/projectData.js
@@ -18,19 +18,19 @@ const projects = [
         projectName: "Casper.com",
         projectURL: "https://casper.com/",
         projectImages: [CasperHomePage, CasperNavMenu, CasperPLP, CasperPDP],
-        description: "Our project involved a comprehensive overhaul of the client's existing website,with a dual focus on modernizing its appearance and transitioning from Demandware to SFRA infrastructure. As a member of the development team, my responsibilities included the construction and upkeep of crucial components such as the mini cart/cart pages, checkout page, and global navigation bar, ensuring seamless functionality across both desktop and mobile platforms. Leveraging GitHub and Bitbucket repositories, I facilitated collaborative development efforts and maintained active codebases to streamline project progress. Midway through the six-month project timeline, the client opted to streamline functionality by temporarily removing the account creation feature. This strategic decision enabled us to adhere to the established site release schedule without compromising quality. Following the successful site launch in early January, the managed services team seamlessly reintegrated the account creation feature, ensuring a smooth transition and optimal user experience for visitors."
+        description: ['Global Navigation', 'Mini-Cart', 'Checkout Page', 'Account Features', 'Front-end UI Bugfixes']
     },
     {
         projectName: "Snipesusa.com",
         projectURL: "https://www.snipesusa.com/",
         projectImages: [SnipesHomePage, SnipesNavMenu, SnipesPLP, SnipesPDP, SnipesBlog],
-        description: "Within the Managed Services team, I played a pivotal role in enhancing the client's website by implementing blog and social media platforms, enriching its interactive capabilities and fostering greater engagement with users. Utilizing GitHub and Bitbucket repositories, I ensured the efficient development and ongoing maintenance of the website's code bases, enabling seamless integration of new features and updates. During my tenure with the managed services team, a client request necessitated the integration of a third-party API, presenting a unique challenge as it was unfamiliar territory for me. However, through effective collaboration with my team and management, we successfully navigated this hurdle within the specified timeframe. This achievement not only bolstered the website's functionality but also underscored our team's adaptability and commitment to meeting client needs promptly and effectively."
+        description: ['3rd Party API Integration', 'Blog Page', 'Backend XML in collaboration with WordPress']
     },
     {
         projectName: "Drapers.com",
         projectURL: 'https://www.drapers.com/',
         projectImages: [DrapersHomePage, DrapersNavMenu, DrapersPLP, DrapersPDP],
-        description: "At Drapers.com, I played a crucial role in a project aimed at optimizing the website's functionality and user experience, utilizing SFCC (Salesforce Commerce Cloud) SFRA (Storefront Reference Architecture) technologies. Utilizing SFCC's Page Designer, I crafted visually appealing pages for both Product Listing Pages (PLP) and Product Detail Pages (PDP), ensuring a cohesive and engaging user experience throughout the website. Additionally, I effectively managed code development and maintenance using GitHub and Bitbucket repositories, facilitating collaboration and ensuring the smooth evolution of the website's capabilities."
+        description: ['Page Designer carousels for PDP Page', 'Page Designer carousels for PLP Page', 'Page Designer carousels for Checkout Page', 'PLP Refinement Options', 'Front-end UI Bugfixes']
     }
     // Add more project objects as needed
 ];
